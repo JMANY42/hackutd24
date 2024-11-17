6bafc16a3f34f8156a8609b1a1d6e4444a8ca4f3
Jonathan Lewis <47508347+JMANY42@users.noreply.github.com>
2024-11-17 09:40:13 -0600

    Update upload.js

--- frontend/src/pages/upload.js
+++ frontend/src/pages/upload.js
@@ -75,7 +75,7 @@ const Upload = () => {
 
             try {
                 // Send the file to the backend
-                const response = await fetch("hackutd24.vercel.app/upload", {
+                const response = await fetch("https://hackutd24.vercel.app/upload", {
                     method: "POST",
                     body: formData,
                 });
